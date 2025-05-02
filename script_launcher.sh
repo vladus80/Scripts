6d5c42f7a313f1452a48ad585dc0e42a7b656ac1
vladus80 <73401455+vladus80@users.noreply.github.com>
2025-05-02 22:20:30 +0300

    Update script_launcher.sh

--- script_launcher.sh
+++ script_launcher.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 set -e
 
+DEBUG=1
 SCRIPT_URL="https://raw.githubusercontent.com/vladus80/Scripts/main/testscript.sh"
 
-# Попытаться использовать curl, wget или sh + /dev/tcp
+debug() {
+  [ "$DEBUG" = "1" ] && echo "$@"
+}
+
 if command -v curl >/dev/null 2>&1; then
-  echo "Запускаем с помощью curl"
+  debug "Запускаем с помощью curl"
   curl -fsSL "$SCRIPT_URL"
 elif command -v wget >/dev/null 2>&1; then
-  echo "Запускаем с помощью wget"
+  debug "Запускаем с помощью wget"
   wget -O- "$SCRIPT_URL"
 elif command -v fetch >/dev/null 2>&1; then
-  echo "Запускаем с помощью fetch"
+  debug "Запускаем с помощью fetch"
   fetch -o - "$SCRIPT_URL"
 elif command -v python3 >/dev/null 2>&1; then
-  echo "Запускаем с помощью python"
+  debug "Запускаем с помощью python"
   python3 -c "import sys, urllib.request; print(urllib.request.urlopen(sys.argv[1]).read().decode(), end='')" "$SCRIPT_URL"
 else
   echo "Ошибка: не найдено ни curl, ни wget, ни fetch, ни python3." >&2
